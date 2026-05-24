4ac00db4d6856c68df443f9ad3adebe05bc3d448
SalmaanShire365 <salmaanshire@gmail.com>
2026-05-24 07:52:29 -0500

    Refresh + Add Sitemap config issues

--- app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { siteMetadata } from "@/lib/data";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${siteMetadata.url}/sitemap.xml`,
-  };
-}
